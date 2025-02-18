a4d80861cdae0e49330a7b04632f43991059851d
Karthik Bandagonda <32044378+Karthik99999@users.noreply.github.com>
2025-02-17 23:20:53 -0700

    Fix build

--- src/js/data/sets/gen9.js
+++ src/js/data/sets/gen9.js
@@ -2,5 +2,5 @@ var SETDEX_SV = {
   "Chillet": {
     "Choice Band": {"ability":"Refrigerate","item":"Choice Band","nature":"Adamant","teraType":"Ice","evs":{"at":252,"df":4,"sp":252},"moves":["Draco Barrage","Extreme Speed","Double Edge","Knock Off"]},
     "Groundium Z": {"ability":"Refrigerate","item":"Groundium Z","nature":"Adamant","teraType":"Ice","evs":{"at":252,"df":4,"sp":252},"moves":["Draco Barrage","Extreme Speed","Dig","Knock Off"]}
-  },
+  }
 };
